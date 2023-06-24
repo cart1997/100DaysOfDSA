e3630e1a8fb94bb561363f46e239c6669999bd4f
cart1997 <kn329@njit.edu>
2023-06-23 23:55:02 -0400

    Initial Commit

--- Day4/src/SearchInRotatedArray.java
+++ Day4/src/SearchInRotatedArray.java
@@ -1,2 +1,46 @@
 public class SearchInRotatedArray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,5,6,7,0,1,2};
+
+        int target = 0;
+
+        int left = 0;
+        int right = nums.length-1;
+
+
+        while (left<=right){
+            int mid = (left+right)/2 ;
+
+            if ( nums[mid] == target ) {
+                System.out.println(mid);
+            }
+
+            if (nums[left]<=nums[mid]){
+                if(nums[left] <= target && nums[mid] >= target){
+                    right= mid -1;
+
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+
+            else {
+
+                if(nums[right] >= target && nums[mid] <= target){
+                    left= mid +1;
+                }
+
+                else {
+
+                    right = mid - 1;
+                }
+
+            }
+
+        }
+
+    }
 }
