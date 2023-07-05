77779652f7c42c304799cf0acb0cee6e24780465
cart1997 <kn329@njit.edu>
2023-07-04 23:47:17 -0400

    Readme.md

--- Day5/src/Search2DArray.java
+++ Day5/src/Search2DArray.java
@@ -1,23 +1,32 @@
 public class Search2DArray {
     public static void main(String[] args) {
 
-        int[][] arr = {{1,2,3,4},{10,11,16,20}};
+        int[][] arr = {{1,2,3,4},{10,11,16,20},{23,30,34,60}};
         int target = 3 ; // check if target is there are not
 
         int low = 0;
-        int high = arr[arr.length-1][arr[0].length-1];
-
-        System.out.println(high);
+        int m = arr.length;
+        int n = arr[0].length;
+        int high = (m*n) - 1;
 
         //binary search
 
         while (low<=high){
-            int mid = low + (high-low)/2;
 
-            if (arr[low][high] == target){
+            int mid = low+(high-low)/2;
+
+            int r = mid/n;
+            int c = mid%n;
+
+            if (arr[r][c] == target){
                 System.out.println(true);
-            } else if (arr[low][high]<target) {
+            } else if (arr[r][c]<target) {
 
+                low = mid +1;
+            }
+            else {
+
+                high = mid-1;
             }
 
         }
