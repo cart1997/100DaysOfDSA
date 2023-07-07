c18156b93570b0c26ad265a451a003ea85917e0b
cart1997 <kn329@njit.edu>
2023-07-06 23:02:12 -0400

    Initial Commit

--- Day6/src/FindMinimum.java
+++ Day6/src/FindMinimum.java
@@ -1,2 +1,35 @@
 public class FindMinimum {
+
+    public int findMin(int[] nums) {
+
+        int low = 0;
+        int high = nums.length-1;
+
+        while (low<=high){
+
+            if(nums[low]<=nums[high]) return nums[low];
+            int mid = low + (high-low)/2;
+
+            if((mid == 0 || nums[mid]<nums[mid-1]) && (mid == nums.length-1 || nums[mid]<nums[mid+1])){
+                return nums[mid];
+            }
+            else if (nums[low]<=nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+        FindMinimum fm = new FindMinimum();
+        int op = fm.findMin(new int[]{3, 4, 5, 1, 2});
+
+        System.out.println(op);
+
+    }
+
 }
--- Day6/src/FirstAndLastIndex.java
+++ Day6/src/FirstAndLastIndex.java
@@ -1,2 +1,84 @@
 public class FirstAndLastIndex {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int first = bsFirst(nums, target);
+        int last = bsLast(nums,target);
+        return new int[]{first,last};
+    }
+    private int bsFirst(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target){
+
+                if(mid == 0 || nums[mid]>nums[mid-1]){
+                    return mid;
+
+                }
+                else{
+                    high = mid-1;
+
+                }
+
+            }
+
+            else if(nums[mid]<target){
+
+                low = mid + 1;
+            }
+            else{
+
+                high = mid - 1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    private int bsLast(int[] nums, int target){
+
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<=high){
+
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target){
+
+                if(mid == nums.length-1 || nums[mid]<nums[mid+1]){
+                    return mid;
+                }
+                else{
+                    low = mid+1;
+                }
+
+            }
+
+            else if(nums[mid]<target){
+
+                low = mid + 1;
+            }
+            else{
+
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        FirstAndLastIndex range = new FirstAndLastIndex();
+
+        range.searchRange(new int[]{5, 7, 7, 8, 8, 1, 5},8);
+
+    }
 }
