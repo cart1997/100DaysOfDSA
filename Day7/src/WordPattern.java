26c27712dfe96752587218839a9a06f3bc4e3821
cart1997 <kn329@njit.edu>
2023-07-09 21:41:31 -0400

    Initial Commit

--- Day7/src/WordPattern.java
+++ Day7/src/WordPattern.java
@@ -1,2 +1,44 @@
+import java.util.HashMap;
+
 public class WordPattern {
+
+    public static void main(String[] args) {
+
+        String pattern = "abba";
+        String s = "dog cat cat dog";
+
+        String[] sarray = s.split(" ");
+
+
+        HashMap<Character,String>  hm = new HashMap<>();
+
+        HashMap<String,Character> hm2 = new HashMap<>();
+
+        for (int i =0; i<pattern.length();i++){
+
+            char c = pattern.charAt(i);
+            String str = sarray[i];
+
+            if (hm.containsKey(c)){
+                if (!hm.get(c).equals(str)){
+                    System.out.println(false);
+                }
+            }
+            else {
+                hm.put(c,str);
+
+            }
+
+            if (hm2.containsKey(str)){
+                if (hm2.get(str)!=c){
+                    System.out.println(false);
+                }
+            }
+            else {
+                hm2.put(str,c);
+            }
+
+        }
+
+    }
 }
