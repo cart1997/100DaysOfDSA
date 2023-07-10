943d48ee601e85920276bd3a248ecf1bc6ab39e3
cart1997 <kn329@njit.edu>
2023-07-09 22:15:21 -0400

    Initial Commit

--- Day7/src/MissingNumber.java
+++ Day7/src/MissingNumber.java
@@ -1,2 +1,22 @@
 public class MissingNumber {
+
+    public static void main(String[] args) {
+
+       int[] arr = {1, 2, 3,4,6};
+
+       int low = 0;
+       int high = arr.length -1;
+
+       while (high-low>=2){
+
+           int mid = low + (high -low)/2;
+           if ((arr[low] - low == 1) != (arr[mid] - mid  == 1)){
+               high =mid;
+           }
+           else {
+               low =mid;
+           }
+       }
+        System.out.println(high+low/2);
+    }
 }
