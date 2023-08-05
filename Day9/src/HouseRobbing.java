4238fd791fa223ef38b266d5eb0c9b7d1e6fb140
cart1997 <kn329@njit.edu>
2023-08-04 22:01:59 -0400

    Initial Commit

--- Day9/src/HouseRobbing.java
+++ Day9/src/HouseRobbing.java
@@ -1,2 +1,6 @@
 public class HouseRobbing {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        int op;
+    }
 }
---
+++ Java2D/src/Array2d.java
@@ -0,0 +1,9 @@
+public class Array2d {
+    public static void main(String[] args) {
+        int[][] op =new int[3+1][11+1];
+
+        System.out.println(op[0].length);
+
+
+    }
+}
