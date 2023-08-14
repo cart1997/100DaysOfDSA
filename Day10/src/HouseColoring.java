060abe16031a4e97ae051039176fede51ec1653c
cart1997 <kn329@njit.edu>
2023-08-13 22:56:09 -0400

    Initial Commit

--- Day10/src/HouseColoring.java
+++ Day10/src/HouseColoring.java
@@ -1,9 +1,69 @@
-public class HouseColoring {
-    public static void main(String[] args) {
-        int[][] costs = {{17,2,17},{16,16,5},{14,3,19}};
+class HouseColoring {
 
-        int l = costs.length;
+    public int minCost(int[][] costs) {
 
-        System.out.println(l-2);
+        int case1 =  helper(costs,0, 0, 0);
+
+        int case2 =  helper(costs,0, 0, 1);
+
+        int case3 =  helper(costs,0, 0, 2);
+
+        return Math.min(case1, Math.min(case2, case3));
+
     }
+
+    private int helper(int[][] costs, int min, int row, int lastColor){
+
+        // base
+
+        if(row == costs.length) return min;
+
+        // logic
+
+        int case1 = Integer.MAX_VALUE;
+
+        int case2 = Integer.MAX_VALUE;
+
+        int case3 = Integer.MAX_VALUE;
+
+        if(lastColor == 0){
+
+            case1= Math.min (
+
+                    helper(costs,min + costs[row][1], row + 1, 1),// blue
+
+                    helper(costs,min + costs[row][2], row + 1, 2)// green
+
+            );
+
+        }
+
+        if(lastColor == 1){
+
+            case2= Math.min (
+
+                    helper(costs,min + costs[row][0], row + 1, 0),// blue
+
+                    helper(costs,min + costs[row][2], row + 1, 2)// green
+
+            );
+
+        }
+
+        if(lastColor == 2){
+
+            case3= Math.min (
+
+                    helper(costs,min + costs[row][0], row + 1, 0),// blue
+
+                    helper(costs,min + costs[row][1], row + 1, 1)// green
+
+            );
+
+        }
+
+        return Math.min(case1, Math.min(case2, case3));
+
+    }
+
 }
