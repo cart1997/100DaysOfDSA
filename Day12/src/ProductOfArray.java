fb6f43b18ceeb228d1a34443a50c362c4cf700be
cart1997 <kn329@njit.edu>
2023-08-27 22:51:07 -0400

    Initial Commit

--- Day12/src/ProductOfArray.java
+++ Day12/src/ProductOfArray.java
@@ -1,2 +1,24 @@
 public class ProductOfArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] res = new int [nums.length];
+
+        int temp = 1;
+        res[0] = temp;
+
+        for(int i =1; i<nums.length;i++){
+            temp = temp*nums[i-1];
+            res[i] = temp;
+        }
+
+        //right
+
+        temp = 1;
+        // res[nums.length-1] =1;
+        for(int i=nums.length-2;i>=0;i--){
+            temp = temp*nums[i+1];
+            res[i] = res[i]*temp;
+            System.out.println(i);
+        }
+    }
 }
