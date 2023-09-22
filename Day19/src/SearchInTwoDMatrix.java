d274df853b6700cff4df9bc1653ee2bf9871d609
cart1997 <kn329@njit.edu>
2023-09-21 23:03:09 -0400

    Initial Commit

--- Day19/src/SearchInTwoDMatrix.java
+++ Day19/src/SearchInTwoDMatrix.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchInTwoDMatrix {
     public static void main(String[] args) {
 
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int i =0;
+        int j = n-1;
+
+        while(i<m && j>=0){
+
+            if(matrix[i][j]==target) return true;
+
+            if(matrix[i][j]<target){
+                i++;
+            }
+            else {
+                j--;
+            }
+
+        }
     }
 }
