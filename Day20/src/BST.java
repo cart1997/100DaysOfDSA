7d82f8d37343cb768bb2455a8550b844880c616d
cart1997 <kn329@njit.edu>
2023-09-25 22:42:29 -0400

    Initial Commit

--- Day20/src/BST.java
+++ Day20/src/BST.java
@@ -1,2 +1,34 @@
+import javax.swing.tree.TreeNode;
+
 public class BST {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public boolean isValidBST(TreeNode root) {
+
+        inorder(root);
+        return true;
+    }
+    private void inorder(TreeNode root){
+        if(root == null) return;
+
+        inorder(root.left);
+
+        inorder(root.right);
+
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
